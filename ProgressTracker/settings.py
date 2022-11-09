a2d1bdac65826510397a099bdadb1035328b76a0
Vishnu Menon <vishnusreemenon@gmail.com>
2022-11-10 00:34:05 +0530

    new commit

--- ProgressTracker/settings.py
+++ ProgressTracker/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sv@z5%%zf^t$c9*q^zg-853t=o3i6j0b1pi=m^hx8q4^9noujg'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
